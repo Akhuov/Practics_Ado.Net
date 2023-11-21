7decedf5360b92180a76e8949b66721a093ed88d
user <user@WIN-F7NIMF7A3VO>
2023-11-21 07:11:15 +0500

    Добавьте файлы проекта.

---
+++ Config.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+
+namespace Practics__001_Ado.Net;
+
+public class Config
+{
+    public void SafeToConfigConnectionString()
+    {
+        try
+        {
+            var setting = new ConnectionStringSettings
+            {
+                Name = "ConString",
+                ConnectionString =
+                    @"Server = WIN-F7NIMF7A3VO;
+                        Database = BootCamp_N7;
+                        Trusted_Connection = True;
+                        TrustServerCertificate = true;
+                        Pooling = True"
+            };
+            Configuration configuration;
+            configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            configuration.ConnectionStrings.ConnectionStrings.Add(setting);
+            configuration.Save();
+        }
+        catch
+        {
+            Console.WriteLine("Error!!");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Practics__001_Ado.Net;
+using System.Configuration;
+
+
+//var r = new Config();
+//r.SafeToConfigConnectionString();
+
+
+string? strOfCon = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+Console.WriteLine(strOfCon);
